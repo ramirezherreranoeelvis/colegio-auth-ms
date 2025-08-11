9be800b77949d61d1b61a757552cb602616b1ecf
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-08-11 03:02:11 -0500

    feat: report notification kafka for student register

---
+++ src/main/java/com/authms/infrastructure/input/rest/dto/RegisterStudentRequest.java
@@ -0,0 +1,51 @@
+package com.authms.infrastructure.input.rest.dto;
+
+import com.authms.infrastructure.input.rest.validators.PhoneNumber;
+import com.authms.infrastructure.input.rest.validators.ValueOfEnum;
+import com.authms.infrastructure.output.persistence.enums.RolUser;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+      @NotBlank(message = "El nombre del usuario es obligatorio")
+      private String name;
+
+      @Pattern(
+            regexp = "^[\\p{L}]+(?:[\\p{L} '\\-]*[\\p{L}]+)*$",
+            message = "Solo se admiten letras, espacios, guiones o apóstrofes; no números"
+      )
+      @NotBlank(message = "El apellido Paterno del usuario es obligatorio")
+      private String surnamePaternal;
+
+      @Pattern(
+            regexp = "^[\\p{L}]+(?:[\\p{L} '\\-]*[\\p{L}]+)*$",
+            message = "Solo se admiten letras, espacios, guiones o apóstrofes; no números"
+      )
+      @NotBlank(message = "El apellido Materno del usuario es obligatorio")
+      private String surnameMaternal;
+
+      @PhoneNumber
+      private String phone;
+
+      @Digits(integer = 8, fraction = 0, message = "El dni debe tener 8 digitos")
+      @NotNull(message = "El dni del usuario es obligatorio")
+      private Integer dni;
+
+      @ValueOfEnum(enumClass = RolUser.class, message = "El rol de usuario es obligatorio")
+      private RolUser rol;
+
+      @Digits(integer = 8, fraction = 0, message = "El dni del padre debe tener 8 digitos")
+      private Integer dniFather;
+      @Digits(integer = 8, fraction = 0, message = "El dni de la madre debe tener 8 digitos")
+      private Integer dniMother;
+      @Digits(integer = 8, fraction = 0, message = "El dni del representante debe tener 8 digitos")
+      private Integer dniRepresentative;
+
+}
